f9f711626ec42bad9d77419f3558009c9f752556
AmauryGuillin <amauryg@hotmail.fr>
2023-01-31 10:52:34 +0100

    first commit worldB

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -6,7 +6,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World WorldA!");
+            Console.WriteLine("Hello World WorldB!");
         }
     }
 }
